9805a15f1eca77fcf3aaaecd6a8e4d1f9797c9b2
Alpamys <alpamys.kanibetov@gmail.com>
2015-01-15 00:26:18 +0600

    first demo, fake data without passing

--- scripts/directives/angular-paginate/angular.paginate.js
+++ scripts/directives/angular-paginate/angular.paginate.js
@@ -6,9 +6,9 @@ app.directive("paginate", ['$http', '$state', '$rootScope',
             restrict: "E",
             replace: true,
             transclude: true,
-            scope:{
-                type: "@",
-                componentModel: "="
+            scope: {
+            /*    type: "@",
+                componentModel: "="*/
             },
 
             controller: function($scope){
@@ -17,11 +17,94 @@ app.directive("paginate", ['$http', '$state', '$rootScope',
             },
 
             link: function (scope, elm, attrs) {
+                scope.pagination = [];
+                scope.controls = [];
+
+                scope.current = {};
+                scope.current.page = 0;
+
+                var first = 0;
+                var last = 0;
+
+                var interval = 10;
+                scope.interval = interval;
+
+                scope.data = [];
+    
+                for (var i=0; i<50; i++) {
+                    scope.data.push(i);  
+                }
+
+                scope.elements = [];
+
+                function process(index) {
+
+                    /*if (index === scope.current.page) {
+                        return;
+                    }*/
+
+                    if (index < 0 || index >= scope.pagination.length) {
+                        return;
+                    }
+
+                    scope.current.page = index;
+
+                    var findIndex = scope.pagination.map(function(e) {
+                        return e.index;
+                    }).indexOf(scope.current.page);
+
+                    if (findIndex !== -1) {
+                        var b = scope.pagination[findIndex];
+                        scope.elements = scope.data.slice(b.begin, b.end);
+                    }
+                }
+
+                scope.page = process;
+
+                function paginate(n) {
+
+                  scope.pagination = [];
+
+                  var index = 0;
+                  for (var i = 0; i < n; i += scope.interval) {
+                    var page = {};
+                    page.begin = i;
+                    page.end = i + scope.interval;
+                    page.index = index++;
+
+                    scope.pagination.push(page);
+                  }
+
+                  if ( scope.current.page >= 0 && scope.current.page < 7 ) {
+                    first = 0;
+
+                    if (n < 10) {
+                      last = n;
+                    }
+                    else {
+                      last = 10;
+                    }
+                  }
+                  else {
+                    last = scope.current.page + 5;
+
+                    while ( last > (n-1) ) {
+                      last--;
+                    }
+
+                    first = last - 10;
+                    while ( first < 0 ) {
+                      first++;
+                    }
+                  }
+
+                  scope.controls = scope.pagination.slice(first, last);
+
+                  process(scope.current.page);
+                }
+
+                paginate(scope.data.length);
             }
         };
-        
-        /*return {
-            template: 'Name:'
-        };*/
     }
 ]);
